ed5b78f0c42df5496546166acbd35ba26454710c
Le Hai Lam <lehailamltk@gmail.com>
2024-03-15 18:03:36 +0700

    LHL

---
+++ script.js
@@ -0,0 +1,40 @@
+// BackTop
+    // Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+// End BackTop
+
+const add = document.querySelector('.inner-buy-icon2-icon');
+add.addEventListener('click', function() {
+  console.log("click");
+});
+
+//Section-two
+const buttonReduce = document.querySelector('.reduce');
+const buttonAdd = document.querySelector('.add');
+const cardInput = document.querySelector('.card_input');
+
+buttonReduce.onclick = function() {
+    cardInput.value = parseInt(cardInput.value) - 1;
+}
+buttonAdd.onclick = function() {
+    cardInput.value = parseInt(cardInput.value) + 1;    
+}
+
+//End section-two
